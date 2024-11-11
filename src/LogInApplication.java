ae3c87a8394ee4d2abd801b465f1a523c33477e4
Daryl <darylvincentgonzales@gmail.com>
2024-11-12 01:21:19 +0800

    testing ulit

--- src/LogInApplication.java
+++ src/LogInApplication.java
@@ -3,7 +3,6 @@ import javax.swing.SwingUtilities;
 public class LogInApplication {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new LogInGUI()); // Constructor
-        //HELLO
-
+        //ELLO
     }
 }
