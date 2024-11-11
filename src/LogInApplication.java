67377d97aa29062da4fd442d92c622c0120b66f5
Daryl <darylvincentgonzales@gmail.com>
2024-11-12 01:28:18 +0800

    ASDJHBJHADBHAS

--- src/LogInApplication.java
+++ src/LogInApplication.java
@@ -4,5 +4,6 @@ public class LogInApplication {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new LogInGUI()); // Constructor
         //BAGO
+        //SDVNKSVNDS
     }
 }
